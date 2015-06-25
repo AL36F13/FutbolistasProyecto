98514afcc4dfff377a40cbf646289b847d133735
AL36F13 <garetreyes.gr@gmail.com>
2015-06-25 09:41:03 -0500

    uno
    Proyecto Final

---
+++ js/acciones.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+$('#btnmenu').on('tap',function(){
+	navigator.vibrate(1000);
+    });
+	
+ $('#btnmessi').on('tap',function(){
+	 navigator.vibrate(1000);
+    });
+ $('#btncristiano').on('tap',function(){
+	 navigator.vibrate(1000);
+    });
+ $('#btnroony').on('tap',function(){
+	 navigator.vibrate(1000);
+    });
+ $('#btnreus').on('tap',function(){
+	 navigator.vibrate(1000);
+    });
+ $('#btnpique').on('tap',function(){
+	 navigator.vibrate(1000);
+    });
+ $('#btnprincipal').on('tap',function(){
+	 navigator.vibrate(1000);
+    });
+}); 
+});
+
